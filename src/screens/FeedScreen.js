1b754e0c5947562268a2e8b4543ee48e8491e651
Shubham Baldaniya <shubham.baldaniya@bacancy.com>
2022-03-21 16:33:06 +0530

    changed feed screen

--- src/screens/FeedScreen.js
+++ src/screens/FeedScreen.js
@@ -1,21 +1,20 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
 
-const FeedScreen = () =>
-{
-    return(
-        <View style={styles.screen}>
-            <Text>Feed Screen</Text>
-        </View>
-    );
+const FeedScreen = () => {
+  return (
+    <View style={styles.screen}>
+      <Text>In Feed Screen</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    screen: {
-        flex:1,
-        justifyContent: "center",
-        alignItems: "center",
-    },  
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
 
 export default FeedScreen;
